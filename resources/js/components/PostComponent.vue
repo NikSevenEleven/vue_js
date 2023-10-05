04b737de30a76061876b2bae89b53ed7a5d551e5
NikSevenEleven <kursdz@mail.ru>
2023-10-05 19:08:13 +0300

    vue.js_data

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,9 +1,16 @@
 <script setup>
 import SinglePostComponent from "./SinglePostComponent.vue";
+let data =
+    {
+        name: 'Vasia',
+        age: 20,
+        married: true,
+        arr: [3 , 5, 15],
+    }
 </script>
-
 <template>
-<div><SinglePostComponent></SinglePostComponent></div>
+    <div><SinglePostComponent></SinglePostComponent></div>
+    <div>Name:{{data.name}}</div>
 </template>
 
 <style scoped>
